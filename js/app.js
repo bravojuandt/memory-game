876a6e1034f100bbe93abf0f24af8fb78574f79f
Juan Bravo <bravojuandt@gmail.com>
2021-10-18 09:51:45 -0400

    some function

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 /*-------------------------------- Constants --------------------------------*/
-const winningCombos = []
+
   
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let winner,random, board, match
+let winner,secretBox, board, match
 
 
 
@@ -14,7 +14,7 @@ const boardSquare = document.querySelector('.board')
 const allSquares = document.querySelectorAll(".squares")
 const statusMessage = document.querySelector('#message')
 const resetBtn = document.querySelector('#reset-button')
-
+const matchInput = document.querySelector('#match-input')
 
 
 
@@ -34,34 +34,37 @@ init()
 function init () {
 
 match = 1
-random = null
+secretBox= null
 board = [null,null, null, null, null, null, null, null, null,null]
 winner = null
-
-
-}
+secretBox = Math.floor(Math.random() * 5 + 1)
+console.log(secretBox)
 render()
+}
 
 
 function render() {
-  if(match === 1) {
-    
+  if(match === secretBox) {
+    return
       }else if(match === -1) {
     
       }
-  for (let i = 0; i < 10; i++) {
-    if (board[i] === 1) {
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === secretBox) {
       allSquares[i].style.backgroundColor = 'pink'
       allSquares[i].innerText = '1'
     } else if(board[i] === -1) {
       allSquares[i].style.backgroundColor = 'orange'
       allSquares[i].innerText = '2'
-
+    
     }else {
       allSquares[i].style.backgroundColor = 'purple'
     }
   }
 }
+
+console.log(board)
+
 
 function handleClick(event) {
   let i = parseInt(event.target.id);
@@ -73,29 +76,9 @@ function handleClick(event) {
 }
 
 
-getWinner()
 
 
 
-
-function getWinner() {
-  
-  let tieCondition = board.includes(null)
-  if (tieCondition === false && winner === null){
-    winner = 'Tie'
-  }
-  for (let i = 0; i < winningCombos.length; i++) {
-    let total = 0
-    let winningCombo = winningCombos[i]
-    for (let i = 0; i < winningCombo.length; i++) {
-      total += board[winningCombo[i]]
-      let winningValue = Math.abs(total)
-      if (winningValue === 3) {
-        winner = board[winningCombo[0]]
-      }
-    }
-  }return 
-}
 
 
 
