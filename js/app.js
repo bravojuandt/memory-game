6ae9b3ac1ea289c525498ed3d2fbdcedb58341a1
Juan Bravo <bravojuandt@gmail.com>
2021-10-19 09:55:46 -0400

    colors memory

--- js/app.js
+++ js/app.js
@@ -1,62 +1,74 @@
-/*-------------------------------- Constants --------------------------------*/
-
-  
+const squaresArray = ['0','1','2','3','4','5','6','7','8','9'];
 
 
-/*---------------------------- Variables (state) ----------------------------*/
-let winner,box1Value, box1Index, box2value, box2Index
+const colorsArray = ['blue', 'green', 'purple', 'orange', 'red'];
 
 
-
-/*------------------------ Cached Element References ------------------------*/
-
-const board = document.querySelector('.board')
-
-const statusMessage = document.querySelector('#message')
-const resetBtn = document.querySelector('#reset-button')
+let blueArray = [],
+  greenArray = [],
+  purpleArray = [],
+  orangeArray = [],
+  redArray = [];
 
 
+let lastColor = "";
+
+let clicks = 0
+
+let clickedSqaures = [];
+
+let duplicateClicks = false;
+
+let gameStart = false;
 
 
-/*----------------------------- Event Listeners -----------------------------*/
-board.addEventListener("click", function(event) {
+// Event Listener
 
+// document.getElementById('start').addEventListener('click', startGame);
+
+// document.getElementById('reset').addEventListener('click', resetGame);
+
+// listen for each square
+document.getElementById('square0').addEventListener('click', function(){
+  squares('square0')
 })
 
-resetBtn.addEventListener('click', function() {
-
+document.getElementById('square1').addEventListener('click', function(){
+  squares('square1')
+})
+document.getElementById('square2').addEventListener('click', function(){
+  squares('square2')
+})
+document.getElementById('square3').addEventListener('click', function(){
+  squares('square3')
+})
+document.getElementById('square4').addEventListener('click', function(){
+  squares('square4')
+})
+document.getElementById('square5').addEventListener('click', function(){
+  squares('square5')
+})
+document.getElementById('square6').addEventListener('click', function(){
+  squares('square6')
+})
+document.getElementById('square7').addEventListener('click', function(){
+  squares('square7')
+})
+document.getElementById('square8').addEventListener('click', function(){
+  squares('square8')
+})
+document.getElementById('square9').addEventListener('click', function(){
+  squares('square9')
 })
 
-
-/*-------------------------------- Functions --------------------------------*/
-
-init()
-
-
-function init () {
-
-turn = 1
-
-let board = ['A', 'B', 'C', 'D', 'E']
-
-render()
+function startGame() {
+  console.log('game started')
 }
 
-function shuffle(){
-  let boxesToShuffle = [];
-  let boxToAdd;
-  for(let i = i; i < board.length; i++){
-    boxToAdd = board.splice(Math.random() * board.length + 1, 1);
-    boxesToShuffle.push(boxToAdd.toString())
-    boxesToShuffle.push(boxToAdd.toString())
-
-  }
-  shuffle(boxesToShuffle);
+function resetGame() {
+  console.log('game reset')
 }
 
-
-
-
-
-
-
+function squares(square) {
+  console.log(square)
+}
